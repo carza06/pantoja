5ad68d4953c02788c447f109609e979ce135166d
Carlos Zabala <soportezabala@gmail.com>
2023-03-24 15:13:57 -0400

    pr 5

--- resources/views/pdf/cabeceracp.blade.php
+++ resources/views/pdf/cabeceracp.blade.php
@@ -3,7 +3,7 @@
     font-size: 10px;
   }
 </style>
-<div style="margin-bottom: 150px;">
+<div >
                   <table width="50%"  border="1" style="border-spacing: 0;" align="right">
                   <td align="center" style="background-color: blue; color: #ffffff">COMPROBANTE DE PAGO</td>
                   <td style="background-color: blue; color: #ffffff"></td>  
@@ -11,7 +11,7 @@
                       <td >
                         Nro. DE CONTROL:
                       </td>
-                      <td></td>
+                      <td>{{ $comprobante }}</td>
                     </tr>
                     <tr>
                       <td >
@@ -26,11 +26,6 @@
                       </td>
                       <td align="center">{{ $pago->formapago->formapago }}</td>
                     </tr>
-                    <tr>
-                      <td >
-                        NUMERO: 
-                      </td>
-                      <td align="center">{{ $comprobante }}</td>
-                    </tr>
+           
                   </table>
                   </div>
