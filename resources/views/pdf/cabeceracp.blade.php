a04281d96dd724bc034f042a44e21a23380c3ca1
Carlos Zabala <soportezabala@gmail.com>
2023-03-24 15:22:52 -0400

    pr 10

--- resources/views/pdf/cabeceracp.blade.php
+++ resources/views/pdf/cabeceracp.blade.php
@@ -1,14 +1,13 @@
-<div >
+<div style="margin-bottom: 150px;">
                   <table width="50%"  border="1" style="border-spacing: 0;" align="right">
                   <tr>
-                  <td align="center" style="background-color: blue; color: #ffffff">COMPROBANTE DE PAGO</td>
-                  <td style="background-color: blue; color: #ffffff"></td>  
-                  </tr>
+                  <td align="center" style="background-color: blue; color: #ffffff">Comprobante de pago</td>
+                  <td style="background-color: blue; color: #ffffff"></td>  </tr>
                   <tr>
                       <td >
                         Nro. DE CONTROL:
                       </td>
-                      <td>{{ $comprobante }}</td>
+                      <td></td>
                     </tr>
                     <tr>
                       <td >
@@ -16,13 +15,23 @@
                       </td>
                       <td align="center">{{ $pago['fechapago'] }}</td>
                     </tr>
-      
+                    <tr>
+                      <td >
+                        USUARIO: 
+                      </td>
+                      <td align="center">{{ $pago->sesion->usuario->nombre }}</td>
+                    </tr>
                     <tr>
                       <td >
                         FORMA DE PAGO: 
                       </td>
                       <td align="center">{{ $pago->formapago->formapago }}</td>
                     </tr>
-           
+                    <tr>
+                      <td >
+                        NUMERO: 
+                      </td>
+                      <td align="center">{{ $comprobante }}</td>
+                    </tr>
                   </table>
-                  </div><br>
+                  </div>
